72e392154986ddd043104934db92dcfdc763c79b
Youngju Lee <lucylee839@hotmail.com>
2019-07-14 17:53:24 -0700

    set up react router for client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,29 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Login from "./pages/Login";
+import About from "./pages/About";
+import Game from "./pages/Game";
+import Rank from "./pages/Rank";
+import Profile from "./pages/Profile";
+import NoMatch from "./pages/NoMatch";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+// Set up the router
+function App() {
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route path ="/" exact component={Login}></Route>
+          <Route path ="/about" exact component={About}></Route>
+          <Route path ="/game" exact component={Game}></Route>
+          <Route path ="/rank" exact component={Rank}></Route>
+          <Route path ="/profile" exact component={Profile}></Route>
+          <Route component ={NoMatch}></Route>
+        </Switch>
       </div>
-    );
-  }
+    </Router>
+  );
 }
 
 export default App;
+
