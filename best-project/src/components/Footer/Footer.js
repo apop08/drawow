4b93e55e7c5c0e1e26b8ba3e9ca443f4cca32859
Evgenia Semez <esemez@indeed.com>
2019-07-15 12:44:22 -0700

    adding a home page with awesome buttons

---
+++ best-project/src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import './Footer.css';
+
+class Footer extends Component {
+    render() {
+        return <div className='footer'>
+            Evgenia Semez
+        </div>
+    }
+}
+
+export default Footer;
---
+++ best-project/src/components/Nav/Nav.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import './Nav.css';
+
+class Nav extends Component {
+    render() {
+        return <div className='nav'>
+            <div className='logo'></div>
+        </div>
+    }
+}
+
+export default Nav;
