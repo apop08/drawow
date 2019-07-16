7157ec3b4b8f3803924e525b8d9b9824d0a9c474
Youngju Lee <lucylee839@hotmail.com>
2019-07-16 14:24:51 -0700

    created buttons and modals

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,22 @@
 
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Login from "./pages/Login";
-import About from "./pages/About";
-import Game from "./pages/Game";
-import Rank from "./pages/Rank";
-import Profile from "./pages/Profile";
-import NoMatch from "./pages/NoMatch";
-import Canvas from "./components/Canvas"
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import Login from "./pages/Login";
+// import About from "./pages/About";
+// import Game from "./pages/Game";
+// import Rank from "./pages/Rank";
+// import Profile from "./pages/Profile";
+// import NoMatch from "./pages/NoMatch";
+// import Canvas from "./components/Canvas"
+import Nav from "./pages/Nav";
+
 // Set up the router
 function App() {
   return (
-    <Router>
+    // <Router>
       <div>
-        <Switch>
+        <Nav /> 
+        {/* <Switch>
           <Route path ="/" exact component={Login}></Route>
           <Route path ="/about" exact component={About}></Route>
           <Route path ="/game" exact component={Game}></Route>
@@ -21,10 +24,10 @@ function App() {
           <Route path ="/profile" exact component={Profile}></Route>
           <Route path ="/test/canvas" exact component={Canvas}></Route>
           <Route component ={NoMatch}></Route>
-        </Switch>
+        </Switch> */}
 
       </div>
-    </Router>
+    // </Router>
   );
 }
 
---
+++ client/src/pages/Nav.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import Modal, { ModalHeader, ModalBody, ModalFooter } from '../components/Modal/index';
+import './style.css';
+
+class Nav extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modal: false
+    };
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+    this.setState({ modal: !this.state.modal });
+  }
+
+  render() {
+    return (
+      <div className="App d-flex justify-content-center">
+        <button
+          type="button"
+          className="btn btn-secondary"
+          onClick={this.toggle}
+        >
+          About
+        </button>
+        <button
+          type="button"
+          className="btn btn-secondary"
+          onClick={this.toggle}
+        >
+          Game
+        </button>    
+         <button
+          type="button"
+          className="btn btn-secondary"
+          onClick={this.toggle}
+        >
+          Rank
+        </button>     
+        <button
+          type="button"
+          className="btn btn-secondary"
+          onClick={this.toggle}
+        >
+          Profile
+        </button>
+        <Modal isOpen={this.state.modal}>
+          <ModalHeader>
+            <h3>This is modal header</h3>
+            <button
+              type="button"
+              className="close"
+              aria-label="Close"
+              onClick={this.toggle}
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </ModalHeader>
+          <ModalBody>
+            <p>This is modal body</p>
+          </ModalBody>
+          <ModalFooter>
+            <button
+              type="button"
+              className="btn btn-secondary"
+              onClick={this.toggle}
+            >
+              Close
+            </button>
+            <button
+              type="button"
+              className="btn btn-primary"
+              onClick={this.toggle}
+            >
+              Save changes
+            </button>
+          </ModalFooter>
+        </Modal>
+      </div>
+    );
+  }
+}
+
+export default Nav;
