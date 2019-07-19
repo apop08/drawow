24b7a8674f41cb2561594985a46f446abc31c0b6
nursaid <uznet@yahoo.com>
2019-07-18 18:33:03 -0700

    changed sql database structure

--- config/seeds.sql
+++ config/seeds.sql
@@ -1,6 +1,28 @@
 USE draWowDB;
 
-INSERT INTO users (username, password, score)
-VALUES (" ");
+INSERT INTO users (username, password)
+VALUES ("Said", "Password");
 
+INSERT INTO users (username, password)
+VALUES ("Alex", "Password");
+
+INSERT INTO users (username, password)
+VALUES ("Evgenia", "Password");
+
+INSERT INTO users (username, password)
+VALUES ("Lucy", "Password");
+
+
+
+INSERT INTO scores (username, score)
+VALUES ("Said", score);
+
+INSERT INTO scores (username, score)
+VALUES ("Lucy", score);
+
+INSERT INTO scores (username, score)
+VALUES ("Alex", score);
+
+INSERT INTO scores (username, score)
+VALUES ("Evgenia", score);
 
