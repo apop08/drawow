0c1a59aac6f41f3b3e7bd7ebe6a9c9e6a8d3ec88
Youngju Lee <lucylee839@hotmail.com>
2019-07-23 18:52:28 -0700

    random word pick working

--- client/src/components/Game/components/GameInfo/RandomWord/index.js
+++ client/src/components/Game/components/GameInfo/RandomWord/index.js
@@ -17,15 +17,9 @@ class RandomWord extends Component {
     getWord (){
         let list = this.state.words.words.split(" ");
         console.log(list)
-        // console.log(list[Math.floor(Math.random()*list.length)])
         return list[Math.floor(Math.random()*list.length)]
     }
     componentDidMount() {
-        // this.getWord();
-        // this.state.words = this.state.words.split(",");
-        // console.log(this.state.words);
-        // console.log(this.getWord());
-        // let word = this.getWord();
         this.setState({word : this.getWord()})
     }
 
