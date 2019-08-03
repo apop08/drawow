ba3d03f7b5bd629dfcc4206c5c2e2b3bc348d685
Alex Poplawski <alex0poplawski@gmail.com>
2019-08-03 10:55:59 -0700

    timer stuff

--- client/src/components/Game/components/GameInfo/Timer/Timer.js
+++ client/src/components/Game/components/GameInfo/Timer/Timer.js
@@ -9,210 +9,41 @@ class Timer extends Component {
             time: this.props.timer
         };
     }
+    componentDidMount() {
+        var loader = document.getElementById('loader')
+            , border = document.getElementById('border')
+            , α = 0
+            , π = Math.PI
+            , t = 30;
+
+        (function draw() {
+            α++;
+            α %= 360;
+            var r = (α * π / 180)
+                , x = Math.sin(r) * 125
+                , y = Math.cos(r) * - 125
+                , mid = (α > 180) ? 1 : 0
+                , anim = 'M 0 0 v -125 A 125 125 1 '
+                    + mid + ' 1 '
+                    + x + ' '
+                    + y + ' z';
+            //[x,y].forEach(function( d ){
+            //  d = Math.round( d * 1e3 ) / 1e3;
+            //});
+
+            loader.setAttribute('d', anim);
+            border.setAttribute('d', anim);
+
+            setTimeout(draw, t); // Redraw
+        })();
+
+    }
 
     render() {
-        return <div>
-            
-            <div className="Timercontainer">
-                <ul className="flip minutePlay">
-                    <li>
-                        <a href="#">
-                            <div className="up">
-                                <div className="shadow"></div>
-                                <div className="inn">0</div>
-                            </div>
-                            <div className="down">
-                                <div className="shadow"></div>
-                                <div className="inn">0</div>
-                            </div>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <div className="up">
-                                <div className="shadow"></div>
-                                <div className="inn">1</div>
-                            </div>
-                            <div className="down">
-                                <div className="shadow"></div>
-                                <div className="inn">1</div>
-                            </div>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <div className="up">
-                                <div className="shadow"></div>
-                                <div className="inn">2</div>
-                            </div>
-                            <div className="down">
-                                <div className="shadow"></div>
-                                <div className="inn">2</div>
-                            </div>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <div className="up">
-                                <div className="shadow"></div>
-                                <div className="inn">3</div>
-                            </div>
-                            <div className="down">
-                                <div className="shadow"></div>
-                                <div className="inn">3</div>
-                            </div>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <div className="up">
-                                <div className="shadow"></div>
-                                <div className="inn">4</div>
-                            </div>
-                            <div className="down">
-                                <div className="shadow"></div>
-                                <div className="inn">4</div>
-                            </div>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <div className="up">
-                                <div className="shadow"></div>
-                                <div className="inn">5</div>
-                            </div>
-                            <div className="down">
-                                <div className="shadow"></div>
-                                <div className="inn">5</div>
-                            </div>
-                        </a>
-                    </li>
-                </ul>
-                <ul className="flip secondPlay">
-                    <li>
-                        <a href="#">
-                            <div className="up">
-                                <div className="shadow"></div>
-                                <div className="inn">0</div>
-                            </div>
-                            <div className="down">
-                                <div className="shadow"></div>
-                                <div className="inn">0</div>
-                            </div>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <div className="up">
-                                <div className="shadow"></div>
-                                <div className="inn">1</div>
-                            </div>
-                            <div className="down">
-                                <div className="shadow"></div>
-                                <div className="inn">1</div>
-                            </div>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <div className="up">
-                                <div className="shadow"></div>
-                                <div className="inn">2</div>
-                            </div>
-                            <div className="down">
-                                <div className="shadow"></div>
-                                <div className="inn">2</div>
-                            </div>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <div className="up">
-                                <div className="shadow"></div>
-                                <div className="inn">3</div>
-                            </div>
-                            <div className="down">
-                                <div className="shadow"></div>
-                                <div className="inn">3</div>
-                            </div>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <div className="up">
-                                <div className="shadow"></div>
-                                <div className="inn">4</div>
-                            </div>
-                            <div className="down">
-                                <div className="shadow"></div>
-                                <div className="inn">4</div>
-                            </div>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <div className="up">
-                                <div className="shadow"></div>
-                                <div className="inn">5</div>
-                            </div>
-                            <div className="down">
-                                <div className="shadow"></div>
-                                <div className="inn">5</div>
-                            </div>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <div className="up">
-                                <div className="shadow"></div>
-                                <div className="inn">6</div>
-                            </div>
-                            <div className="down">
-                                <div className="shadow"></div>
-                                <div className="inn">6</div>
-                            </div>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <div className="up">
-                                <div className="shadow"></div>
-                                <div className="inn">7</div>
-                            </div>
-                            <div className="down">
-                                <div className="shadow"></div>
-                                <div className="inn">7</div>
-                            </div>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <div className="up">
-                                <div className="shadow"></div>
-                                <div className="inn">8</div>
-                            </div>
-                            <div className="down">
-                                <div className="shadow"></div>
-                                <div className="inn">8</div>
-                            </div>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <div className="up">
-                                <div className="shadow"></div>
-                                <div className="inn">9</div>
-                            </div>
-                            <div className="down">
-                                <div className="shadow"></div>
-                                <div className="inn">9</div>
-                            </div>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-
-        </div>
+        return <svg width="250" height="250" viewbox="0 0 250 250">
+            <path id="border" transform="translate(125, 125)" />
+            <path id="loader" transform="translate(125, 125) scale(.84)" />
+        </svg>
     }
 }
 export default Timer;  
